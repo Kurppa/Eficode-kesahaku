e8597e38fea2f4931abcf1b5a681716ea596b6a4
Kurppa <peik.feiring@gmail.com>
2020-01-27 18:25:34 +0200

    frontend updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { Container } from 'semantic-ui-react'
+
+import { ITINERARY_BETWEEN_TWO } from './graphql/queries'
+import {
+  eficodeCoordinates as efic,
+  kaisaniemiCoordinates as kaic
+} from './utils/data'
+import Routelisting from './components/Routelisting'
 
 const App = () => {
+  const { data, loading, error } = useQuery(ITINERARY_BETWEEN_TWO, {
+    variables: {
+      from: { lat: efic.lat, lon: efic.lon },
+      to: { lat: kaic.lat, lon: kaic.lon },
+      numItineraries: 10,
+      walkReluctance: 5
+    },
+    pollInterval: 1000 * 60
+  })
+
+  const containerStyle = {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    flexShrink: 1
+  }
+
   return (
-    <div>
-    </div>
+    <Container style={containerStyle}>
+      {
+        !loading && !error
+          ? <Routelisting routes={data.plan.itineraries}/>
+          : ''
+      }
+    </Container>
   )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+import { ApolloProvider } from '@apollo/react-hooks'
+import ApolloClient from 'apollo-boost'
+
+const client = new ApolloClient({
+  uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
+})
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 )
